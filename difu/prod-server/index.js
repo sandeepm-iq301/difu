6521274a956e009559d11ff0f8e1ad56cbfac081
sandeepm-iq301 <sandeepm@iq301.com>
2021-06-25 09:47:58 +0530

    commit

---
+++ difu/prod-server/index.js
@@ -0,0 +1,10 @@
+//const express = require('express')
+import express from 'express';
+var app = express();
+var port = 3000;
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(port, function () {
+  console.log("diFU app listening at http://localhost:".concat(port));
+});
